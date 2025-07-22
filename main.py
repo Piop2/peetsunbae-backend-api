afad9d27cd4f25f7a645bddb3b86802bd60f73bd
piop2 <pioooop2@gmail.com>
2025-07-22 23:17:13 +0900

    update main.py

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ import sunbae_api
 from sunbae_api import Day, Book, Word
 
 BOOK: Book = Book.WORDMASTER_V2018
-TARGET_DAYS: tuple[Day] = (Day(number=1, is_total=True),)
+
+target_days: list[Day] = []
+days_str: list[str] = input(">>> ").split()
+
+for day_str in days_str:
+    for i in range(len(day_str)):
+        if day_str[0:i].isnumeric():
+            continue
+
+        is_total: bool
+        match day_str[i:0]:
+            case "a":
+                is_total = True
+            case "w":
+                is_total = False
+            case _:
+                raise RuntimeError
+
+        target_days.append(Day(int(day_str[0:i]), is_total))
 
 token: str
 with open("config.toml", "rb") as file:
@@ -16,11 +34,11 @@ with open("config.toml", "rb") as file:
 user: sunbae_api.User = sunbae_api.start(token)
 
 words: tuple[Word, ...] = sunbae_api.start_word_tests(
-    user=user, book=BOOK, days=TARGET_DAYS
+    user=user, book=BOOK, days=target_days
 )
 
 for word in words:
     answer: str = word.meaning.split(", ")[0]
     word.answer = re.sub(r"\([^)]*\)|\[[^]]*]", "", answer)
 
-sunbae_api.save_word_test(user=user, book=BOOK, days=TARGET_DAYS, answers=words)
+sunbae_api.save_word_test(user=user, book=BOOK, days=target_days, answers=words)
